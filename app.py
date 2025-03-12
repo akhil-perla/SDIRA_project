27da6559c2d980566fad3e9804309c1acceeb072
Moksh Kothari <moksh_kothari@berkeley.edu>
2025-03-11 21:34:58 -0700

    Updated dashboard to match new files.json format

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from routes.file_upload import file_upload
+from routes.dashboard import dashboard_bp  # Import your dashboard blueprint
 
 app = Flask(__name__)
 app.register_blueprint(file_upload)
+app.register_blueprint(dashboard_bp)  # Register your dashboard
 
 @app.route("/")
 def index():
